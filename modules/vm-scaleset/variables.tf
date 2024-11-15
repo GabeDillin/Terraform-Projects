212be1aa8c64311e5ea758875d4d22f2338532fe
GabeDillin <gabedillin@gmail.com>
2024-11-14 16:15:26 -0800

    Create variables.tf

---
+++ modules/vm-scaleset/variables.tf
@@ -0,0 +1,8 @@
+variable "location" {}
+variable "vmss_rg" {}
+variable "vmss_prefix" {}
+
+variable "vmss_instance" {}
+variable "admin_username" {}
+variable "admin_password" {}
+variable "vmss_subnet_id" {}
